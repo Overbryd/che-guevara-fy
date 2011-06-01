9ef9c6f9f0e3cfadfad6573cb5ea015cf10e944a
Overbryd <l.rieder@gmail.com>
2011-06-02 01:14:21 +0200

    Added google analytics tracking

--- che-guevara-fy.rb
+++ che-guevara-fy.rb
@@ -22,6 +22,16 @@ __END__
     %meta{"http-equiv" => "Content-Type", :content => "text/html; charset=utf-8"}
   %body
     = yield
+    :javascript
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-2373458-14']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
 
 @@ index
 %form{:action => '/che-guevara-fy', :method => 'POST', :enctype => 'multipart/form-data'}
